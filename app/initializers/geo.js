4edb798ce77dc6b50c294763b1c2d8efd056504e
mcamara <mcamara@qbp.com>
2017-04-19 20:26:13 -0500

    Finish Challenge 2. Add initializer for geolocation

---
+++ app/initializers/geo.js
@@ -0,0 +1,32 @@
+// Created by doing 'ember g initializer geo'
+export function initialize(app) {
+  // We get the geolocation api
+  const geo = navigator.geolocation;
+  // We defer our app readiness
+  app.deferReadiness();
+
+  geo.getCurrentPosition((pos) => {
+    let pt = pos.coords;
+    let loc = {
+      lat: pt.latitude,
+      lng: pt.longitude
+    }
+
+    // Add our new object to the app by using 'app.register('key', obj, {config})'
+    // We set "instantiate:false" to prevent Ember from trying to instantiate this object
+    // since this is not a factory. Anywhere in the App where we get a hold of the App's container
+    // the same object will be available
+    app.register('data:location', loc, {
+      instantiate: false
+    });
+
+    // Finally we let our app continue
+    app.advanceReadiness();
+  }); 
+  
+}
+
+export default {
+  name: 'geo',
+  initialize
+};
--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,7 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 // Using our math module
 import {default as math, PI} from 'math';
 
-const { Route, inject } = Ember;
+const { Route, inject, getOwner } = Ember;
 
 export default Route.extend(ApplicationRouteMixin, {
     session: inject.service(),
@@ -16,5 +16,21 @@ export default Route.extend(ApplicationRouteMixin, {
         // Always do this._super(...arguments)
         this._super(...arguments);
         console.log(math, PI);
+    },
+
+    // setupController hook
+    setupController(controller) {
+        this._super(...arguments);
+
+        // Add a new property 'loc' which contains the geolocation information we setupController
+        // in our 'geo' initializer to this controller when its setup by the route.
+        // This will be available directly in the top level template available to this route
+        // NOTE: 'getOwner' is the same as 'Ember.getOwner'
+        controller.set('loc', 
+            // We get the container to which this route belongs
+            getOwner(this)
+                // And we call 'lookup' passing the container key so we get the object stored under that key
+                // and making it available in the template.
+                .lookup('data:location'))    
     }
 });
